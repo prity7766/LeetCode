fae4789d80a82472fdb26127935793406036f982
prity <kumariprity102021@gmail.com>
2026-01-24 07:42:39 +0530

    Time: 18 ms (27.24%) | Memory: 51.1 MB (8.11%) - LeetSync

---
+++ 49-group-anagrams/group-anagrams.java
@@ -0,0 +1,25 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+         HashMap<String, List<String>> map= new HashMap<>();
+        List<List<String>> ans = new ArrayList<>();
+        for(String s: strs){
+            int[] count = new int[26];
+            for (char c :s.toCharArray()){
+                count[c-'a']++;
+            }
+            // Arrays.sort(chrs);
+            String word = Arrays.toString(count);
+            if(map.containsKey(word)){
+            map.get(word).add(s);
+            }else{
+                 List<String> ls = new ArrayList<>();
+                ls.add(s);
+                map.put(word,ls);
+                ans.add(ls);
+            }
+            
+        }
+       
+        return ans;
+    }
+}
