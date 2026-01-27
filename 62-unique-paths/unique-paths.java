97650c5682651c434f2a961726fd9d0c1f9c244b
prity <kumariprity102021@gmail.com>
2026-01-27 06:47:28 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (20.64%) - LeetSync

---
+++ 62-unique-paths/unique-paths.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int uniquePaths(int m, int n) {
+          int[][] dp = new int[m][n];
+        for(int[] i:dp) Arrays.fill(i, -1);
+        return find(0,0 ,dp , m , n);
+        
+        }
+    private int find(int x,  int y , int[][] dp , int m , int n){
+        if(x==m-1 && y==n-1) return 1;
+        else if (x>=m || y>=n) return 0;
+        else if(dp[x][y]!=-1) return dp[x][y];
+        else return dp[x][y]= find(x+1 , y ,dp , m , n)+ find(x , y+1 , dp , m, n);
+    }
+}
