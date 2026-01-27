e24ec625b18ed6864d86b2ac42adc24faa19bdf6
prity <kumariprity102021@gmail.com>
2026-01-27 06:54:37 +0530

    Time: 157 ms (33.80%) | Memory: 42.9 MB (69.64%) - LeetSync

---
+++ 79-word-search/word-search.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean exist(char[][] board, String word) {
+        int n =board.length;
+        int m  = board[0].length;
+    
+        for(int i = 0 ; i <n  ; i++){
+            for(int j = 0 ; j <m ; j++){
+                if(board[i][j]==word.charAt(0) && dfs(board, word, i,j , 0, n,m, new boolean[n][m]))return true;            }
+        }
+             return false;
+
+    }
+
+     private boolean dfs(char[][] board, String word,int i , int j, int index , int n , int m, boolean[][] newvisited){
+           if(index==word.length())return true;            
+            if(i<0  || i>=n || j<0 || j>=m || index==word.length() || newvisited[i][j] || ( board[i][j]!=word.charAt(index) ) ) return false;
+                newvisited[i][j]=true;
+                
+            if(dfs(board, word,i , j+1 , index+1, n,m , newvisited) ||
+             dfs(board, word, i+1 , j , index+1, n,m, newvisited) ||
+            dfs(board, word, i , j-1 , index+1, n,m, newvisited) ||
+            dfs(board, word, i-1 , j , index+1, n,m, newvisited)
+             )return true;
+            newvisited[i][j]=false;
+            
+
+            return false;
+    }
+}
