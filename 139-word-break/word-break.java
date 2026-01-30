f9660f0bf7bac7864d0d610a553d8781684ab138
prity <kumariprity102021@gmail.com>
2026-01-30 07:00:41 +0530

    Time: 6 ms (62.14%) | Memory: 42.5 MB (99.81%) - LeetSync

---
+++ 139-word-break/word-break.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean wordBreak(String s, List<String> wordDict) {
+         boolean[] dp = new boolean[s.length()+1];
+        dp[s.length()]=true;
+        for(int i = s.length()-1 ; i>=0 ; i--){
+            for(String word  : wordDict){
+                if(i+word.length() <=s.length() && s.regionMatches(i, word, 0, word.length()) && dp[i+word.length()]==true) dp[i]=true;
+            }
+        }
+        return dp[0];
+    }
+}
